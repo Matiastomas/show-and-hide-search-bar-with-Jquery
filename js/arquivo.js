5e0cabe63822f6a0fb9caf777dff147f794af506
Matiastomas <matiasgracianotomas@gmail.com>
2019-01-04 19:34:40 +0200

    project-files

---
+++ js/arquivo.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+      //Method to show search  bar
+	$("#show").click(function(){
+
+		$("#search").show("slow");
+        });
+
+    //Method to show search  bar
+       $("#hide").click(function(){
+		
+		$("#search").hide("slow");
+	});
+
+});
