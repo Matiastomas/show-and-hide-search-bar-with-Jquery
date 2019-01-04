568bd28b53435b44bfd32256daaab796814b463f
Matiastomas <matiasgracianotomas@gmail.com>
2019-01-04 21:54:13 +0200

    Removed unnecessary
    At the time I wrote this code was without any comment, I decided to add but I think it is self explanatory

--- js/arquivo.js
+++ js/arquivo.js
@@ -1,11 +1,10 @@
 $(document).ready(function(){
-      //Method to show search  bar
+     
 	$("#show").click(function(){
 
 		$("#search").show("slow");
         });
 
-    //Method to show search  bar
        $("#hide").click(function(){
 		
 		$("#search").hide("slow");
